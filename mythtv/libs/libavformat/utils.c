Cougar/mythtv-cougar

commit 771f0c5df746fff79dcda4b8305ffcb2fcb3305a

A number of small, interrelated changes: - Skip 'dummy' recordings during playback, instead, just pause. - Get rid of the dummydtvrecorder class, since anything it generates would now be skipped. - Add a 'peek' function to the RingBuffer class, for, err, peeking. - Make the 'video/audio is X frames ahead' debugging statements more obvious in what they mean. - Allow switching between xvmc and xv during playback (if the file type changes, not manually). - sync the file to disk more often when first starting recording. - In RingBuffer, re-try to open files more often (but for the same amount of time), and don't spam the console with error messages. - Reset the 'read ahead' bitrate calcs on file change, and add some lower bitrate modes. - Optimize libavformat/utils.c, av_find_stream_info().
git-svn-id: http://svn.mythtv.org/trunk@9820 4b91aca1-2a1f-0410-a73a-a78f2df169e8

mythtv/libs/libavformat/utils.c
@@ -1994,6 +1994,9 @@ static int try_decode_frame(AVStream *st, const uint8_t *data, int size)
 /** Absolute maximum size we read until we abort. */
 #define MAX_READ_SIZE        5000000
 
+/** Number of frames to read, max. */
+#define MAX_FRAMES           15
+
 /** Maximum duration until we stop analysing the stream. */
 #define MAX_STREAM_DURATION  ((int)(AV_TIME_BASE * 2.0))
 
@@ -2010,13 +2013,14 @@ static int try_decode_frame(AVStream *st, const uint8_t *data, int size)
  */
 int av_find_stream_info(AVFormatContext *ic)
 {
-    int i, count, ret, read_size, j;
+    int i, count, ret, read_size, j, read_packets;
     AVStream *st;
     AVPacket pkt1, *pkt;
     AVPacketList *pktl=NULL, **ppktl;
     int64_t last_dts[MAX_STREAMS];
     int64_t duration_sum[MAX_STREAMS];
     int duration_count[MAX_STREAMS]={0};
+    int hasaudio = 0, hasvideo = 0;
 
     for(i=0;i<ic->nb_streams;i++) {
         st = ic->streams[i];
@@ -2062,20 +2066,26 @@ int av_find_stream_info(AVFormatContext *ic)
                 break;
             if(st->parser && st->parser->parser->split && !st->codec->extradata)
                 break;
+            if (st->codec->codec_type == CODEC_TYPE_VIDEO)
+                hasvideo = 1;
+            else if (st->codec->codec_type == CODEC_TYPE_AUDIO)
+                hasaudio = 1;
         }
 
         if (i == ic->nb_streams) {
             /* NOTE: if the format has no header, then we need to read
                some packets to get most of the streams, so we cannot
                stop here */
-            if (!(ic->ctx_flags & AVFMTCTX_NOHEADER)) {
+            if (!(ic->ctx_flags & AVFMTCTX_NOHEADER) ||
+                (read_size >= MAX_READ_SIZE || read_packets >= MAX_FRAMES) ||
+                (hasvideo && hasaudio)) {
                 /* if we found the info for all the codecs, we can stop */
                 ret = count;
                 break;
             }
         } else {
             /* we did not get all the codec info, but we read too much data */
-            if (read_size >= MAX_READ_SIZE) {
+            if (read_size >= MAX_READ_SIZE || read_packets >= MAX_FRAMES) {
                 ret = count;
                 break;
             }
@@ -2096,6 +2106,7 @@ int av_find_stream_info(AVFormatContext *ic)
                 ret = 0;
             break;
         }
+        read_packets++;
 
         pktl = av_mallocz(sizeof(AVPacketList));
         if (!pktl) {

mythtv/libs/libmythtv/NuppelVideoPlayer.cpp
@@ -37,6 +37,7 @@ using namespace std;
 #include "decoderbase.h"
 #include "nuppeldecoder.h"
 #include "avformatdecoder.h"
+#include "dummydecoder.h"
 #include "jobqueue.h"
 #include "DVDRingBuffer.h"
 #include "NuppelVideoRecorder.h"
@@ -234,7 +235,8 @@ NuppelVideoPlayer::NuppelVideoPlayer(QString inUseID, const ProgramInfo *info)
       tc_avcheck_framecounter(0),   tc_diff_estimate(0),
       savedAudioTimecodeOffset(0),
       // LiveTVChain stuff
-      livetvchain(NULL), m_tv(NULL),
+      livetvchain(NULL),            m_tv(NULL), 
+      isDummy(false),               
       // DVD stuff
       indvdstillframe(false), hidedvdbutton(true),
       // Debugging variables
@@ -560,7 +562,8 @@ void NuppelVideoPlayer::ReinitVideo(void)
     videofiltersLock.lock();
 
     float aspect = (forced_video_aspect > 0) ? forced_video_aspect : video_aspect;
-    videoOutput->InputChanged(video_width, video_height, aspect);
+    videoOutput->InputChanged(video_width, video_height, aspect, 
+                              GetDecoder()->GetVideoCodecID());
     if (videoOutput->IsErrored())
     {
         VERBOSE(VB_IMPORTANT, "ReinitVideo(): videoOutput->IsErrored()");
@@ -851,9 +854,32 @@ void NuppelVideoPlayer::SetFileLength(int total, int frames)
     totalFrames = frames;
 }
 
+void NuppelVideoPlayer::OpenDummy(void)
+{
+    isDummy = true;
+
+    if (!videoOutput)
+    {
+        SetVideoParams(720, 576, 25.00, 15);
+    }
+
+    SetDecoder(new DummyDecoder(this, m_playbackinfo));
+}
+
 int NuppelVideoPlayer::OpenFile(bool skipDsp, uint retries,
                                 bool allow_libmpeg2)
 {
+    isDummy = false;
+
+    if (livetvchain)
+    {
+        if (livetvchain->GetCardType(livetvchain->GetCurPos()) == "DUMMY")
+        {
+            OpenDummy();
+            return 0;
+        }
+    }
+
     if (!skipDsp)
     {
         if (!ringBuffer)
@@ -894,23 +920,14 @@ int NuppelVideoPlayer::OpenFile(bool skipDsp, uint retries,
     char testbuf[kDecoderProbeBufferSize];
     ringBuffer->Unpause(); // so we can read testbuf if we were paused
 
-    int readsize = kDecoderProbeBufferSize;
-    if (ringBuffer->isDVD())
-        readsize = 2048;
-
-    if (ringBuffer->Read(testbuf, readsize) != readsize)
+    int readsize = 2048;
+    if (ringBuffer->Peek(testbuf, readsize) != readsize)
     {
         VERBOSE(VB_IMPORTANT,
                 QString("NVP::OpenFile(): Error, couldn't read file: %1")
                 .arg(ringBuffer->GetFilename()));
         return -1;
     }
-
-    // Seek back to begining so that OpenFile can find PAT/PMT more quickly
-    ringBuffer->Pause();
-    ringBuffer->WaitForPause();
-    ringBuffer->Seek(0, SEEK_SET);
-    ringBuffer->Unpause();
 
     // delete any pre-existing recorder
     SetDecoder(NULL);
@@ -971,6 +988,7 @@ int NuppelVideoPlayer::OpenFile(bool skipDsp, uint retries,
     // is true, only disable if no_video_decode is true.
     int ret = GetDecoder()->OpenFile(ringBuffer, no_video_decode, testbuf,
                                      readsize);
+
     if (ret < 0)
     {
         VERBOSE(VB_IMPORTANT, QString("Couldn't open decoder for: %1")
@@ -2017,8 +2035,8 @@ void NuppelVideoPlayer::AVSync(void)
     FrameScanType ps = (m_double_framerate) ? kScan_Interlaced : kScan_Ignore;
     if (diverge < -MAXDIVERGE)
     {
-        // If video is way ahead of audio, adjust for it...
-        QString dbg = QString("Video is %1 frames ahead of audio, ")
+        // If video is way behind of audio, adjust for it...
+        QString dbg = QString("Video is %1 frames behind audio (too slow), ")
             .arg(-diverge);
 
         // Reset A/V Sync
@@ -2038,7 +2056,7 @@ void NuppelVideoPlayer::AVSync(void)
         else
         {
             // If we are using software decoding, skip this frame altogether.
-            VERBOSE(VB_PLAYBACK, LOC + dbg + "dropping frame.");
+            VERBOSE(VB_PLAYBACK, LOC + dbg + "dropping frame to catch up.");
         }
     }
     else if (!using_null_videoout)
@@ -2090,14 +2108,14 @@ void NuppelVideoPlayer::AVSync(void)
 
     if (diverge > MAXDIVERGE)
     {
-        // If audio is way ahead of video, adjust for it...
+        // If audio is way behind of video, adjust for it...
         // by cutting the frame rate in half for the length of this frame
 
         avsync_adjustment = frame_interval;
         lastsync = true;
         VERBOSE(VB_PLAYBACK, LOC + 
-                QString("Audio is %1 frames ahead of video,\n"
-                        "\t\t\tdoubling video frame interval.").arg(diverge));
+                QString("Video is %1 frames ahead of audio,\n"
+                        "\t\t\tdoubling video frame interval to slow down.").arg(diverge));
     }
 
     if (audioOutput && normal_speed)
@@ -2224,6 +2242,13 @@ bool NuppelVideoPlayer::PrebufferEnoughFrames(void)
     prebuffering_lock.lock();
     if (prebuffering)
     {
+        if (!audio_paused && audioOutput)
+        {
+           if (prebuffering)
+                audioOutput->Pause(prebuffering);
+            audio_paused = prebuffering;
+        }
+
         VERBOSE(VB_PLAYBACK, LOC + QString("Waiting for prebuffer.. %1 %2")
                 .arg(prebuffer_tries).arg(videoOutput->GetFrameStatus()));
         if (!prebuffering_wait.wait(&prebuffering_lock,
@@ -2248,6 +2273,7 @@ bool NuppelVideoPlayer::PrebufferEnoughFrames(void)
         }
         prebuffering_lock.unlock();
         videosync->Start();
+
         return false;
     }
     prebuffering_lock.unlock();
@@ -2293,7 +2319,15 @@ void NuppelVideoPlayer::DisplayNormalFrame(void)
     if (!ringBuffer->InDVDMenuOrStillFrame())
     {
         if (!PrebufferEnoughFrames())
+        {
+            // When going to switch channels
+            if (paused)
+            {
+                usleep(frame_interval);
+                DisplayPauseFrame();
+            }
             return;
+        }
     }
 
     videoOutput->StartDisplayingFrame();
@@ -2486,7 +2520,7 @@ void NuppelVideoPlayer::OutputVideoLoop(void)
             } 
         }
 
-        if (pausevideo)
+        if (pausevideo || isDummy)
         {
             usleep(frame_interval);
             DisplayPauseFrame();
@@ -2632,9 +2666,13 @@ void NuppelVideoPlayer::SwitchToProgram(void)
     VERBOSE(VB_PLAYBACK, "SwitchToProgram(void)");
 
     bool discontinuity = false, newtype = false;
-    ProgramInfo *pginfo = livetvchain->GetSwitchProgram(discontinuity, newtype);
+    int newid = -1;
+    ProgramInfo *pginfo = livetvchain->GetSwitchProgram(discontinuity, newtype,
+                                                        newid);
     if (!pginfo)
         return;
+
+    bool newIsDummy = livetvchain->GetCardType(newid) == "DUMMY";
 
     if (m_playbackinfo)
     {
@@ -2647,6 +2685,15 @@ void NuppelVideoPlayer::SwitchToProgram(void)
 
     ringBuffer->Pause();
     ringBuffer->WaitForPause();
+
+    if (newIsDummy)
+    {
+        OpenDummy();
+        ResetPlaying();
+        DoPause();
+        eof = false;
+        return;
+    }
 
     ringBuffer->OpenFile(pginfo->pathname, 10 /* retries -- about 5 seconds */);
     if (!ringBuffer->IsOpen())
@@ -2733,11 +2780,14 @@ void NuppelVideoPlayer::JumpToProgram(void)
 {
     VERBOSE(VB_PLAYBACK, "JumpToProgram(void)");
     bool discontinuity = false, newtype = false;
-    ProgramInfo *pginfo = livetvchain->GetSwitchProgram(discontinuity, newtype);
+    int newid = -1;
+    ProgramInfo *pginfo = livetvchain->GetSwitchProgram(discontinuity, newtype,
+                                                        newid);
     if (!pginfo)
         return;
 
     long long nextpos = livetvchain->GetJumpPos();
+    bool newIsDummy = livetvchain->GetCardType(newid) == "DUMMY";
     
     if (m_playbackinfo)
     {
@@ -2756,6 +2806,16 @@ void NuppelVideoPlayer::JumpToProgram(void)
     livetvchain->SetProgram(pginfo);
 
     ringBuffer->Reset(true);
+
+    if (newIsDummy)
+    {
+        OpenDummy();
+        ResetPlaying();
+        DoPause();
+        eof = false;
+        return;
+    }
+
     ringBuffer->OpenFile(pginfo->pathname);
     if (!ringBuffer->IsOpen())
     {
@@ -2765,10 +2825,14 @@ void NuppelVideoPlayer::JumpToProgram(void)
         return;
     }
 
-    if (newtype)
+    bool wasDummy = isDummy;
+    if (newtype || wasDummy)
         errored = (OpenFile() >= 0) ? errored : true;
     else
         ResetPlaying();
+
+    if (wasDummy)
+        DoPlay();
 
     if (errored || !GetDecoder())
     {
@@ -2964,17 +3028,31 @@ void NuppelVideoPlayer::StartPlaying(void)
     }
     commBreakMapLock.unlock();
 
+    if (isDummy)
+    {
+        DoPause();
+    }
+
     while (!killplayer && !errored)
     {
         if (m_playbackinfo)
             m_playbackinfo->UpdateInUseMark();
 
-        if ((!paused || eof) && livetvchain && !GetDecoder()->GetWaitForChange())
+        if (isDummy && livetvchain && livetvchain->HasNext())
+        {
+            livetvchain->JumpToNext(true, 1);
+            JumpToProgram();
+        }
+        else if ((!paused || eof) && livetvchain && !GetDecoder()->GetWaitForChange())
         {
             if (livetvchain->NeedsToSwitch())
                 SwitchToProgram();
-            else if (livetvchain->NeedsToJump())
-                JumpToProgram();
+        }
+
+        if (livetvchain && livetvchain->NeedsToJump() && 
+            !GetDecoder()->GetWaitForChange())
+        {
+            JumpToProgram();
         }
 
         if (forcePositionMapSync)
@@ -2990,7 +3068,8 @@ void NuppelVideoPlayer::StartPlaying(void)
             break;
         }
 
-        if (play_speed != next_play_speed)
+        if (play_speed != next_play_speed && 
+            (!livetvchain || (livetvchain && !livetvchain->NeedsToJump())))
         {
             decoder_lock.lock();
 
@@ -3023,11 +3102,19 @@ void NuppelVideoPlayer::StartPlaying(void)
                     livetvchain->JumpToNext(true, 1);
                     continue;
                 }
-                VERBOSE(VB_PLAYBACK, "Ignoring livetv eof in decoder loop");
+                if (!paused)
+                    VERBOSE(VB_PLAYBACK, "Ignoring livetv eof in decoder loop");
                 usleep(50000);
             }
             else
                 break;
+        }
+
+        if (isDummy)
+        {
+            decoderThreadPaused.wakeAll();
+            usleep(500);
+            continue;
         }
 
         if (rewindtime < 0)

mythtv/libs/libmythtv/NuppelVideoPlayer.h
@@ -107,6 +107,7 @@ class NuppelVideoPlayer : public CC608Reader, public CC708Reader
     bool InitVideo(void);
     int  OpenFile(bool skipDsp = false, uint retries = 4,
                   bool allow_libmpeg2 = true);
+    void OpenDummy(void);
 
     // Windowing stuff
     void EmbedInWidget(WId wid, int x, int y, int w, int h);
@@ -736,6 +737,7 @@ class NuppelVideoPlayer : public CC608Reader, public CC708Reader
     // LiveTV
     LiveTVChain *livetvchain;
     TV *m_tv;
+    bool isDummy;
 
     // DVD
     bool indvdstillframe;

mythtv/libs/libmythtv/RingBuffer.cpp
@@ -91,7 +91,7 @@ RingBuffer::RingBuffer(const QString &lfilename,
       rbrpos(0),                rbwpos(0),
       internalreadpos(0),       ateof(false),
       readsallowed(false),      wantseek(false), setswitchtonext(false),
-      rawbitrate(8000),         playspeed(1.0f),
+      rawbitrate(4000),         playspeed(1.0f),
       fill_threshold(-1),       fill_min(-1),
       readblocksize(128000),    wanttoread(0),
       numfailures(0),           commserror(false),
@@ -115,7 +115,8 @@ RingBuffer::RingBuffer(const QString &lfilename,
         return;
     }
 
-    OpenFile(filename, read_retries);
+    if (read_retries != (uint)-1)
+        OpenFile(filename, read_retries);
 }
 
 /** \fn check_permissions(const QString&)
@@ -211,10 +212,15 @@ void RingBuffer::OpenFile(const QString &lfilename, uint retryCount)
     if (is_local)
     {
         char buf[kReadTestSize];
-        while (openAttempts > 0)
+        int timetowait = 500 * openAttempts;
+        int lasterror = 0;
+
+        MythTimer openTimer;
+        openTimer.start();
+
+        while (openTimer.elapsed() < timetowait)
         {
-            openAttempts--;
-                
+            lasterror = 0;
             fd2 = open(filename.ascii(), O_RDONLY|O_LARGEFILE|O_STREAMING);
                 
             if (fd2 < 0)
@@ -222,34 +228,43 @@ void RingBuffer::OpenFile(const QString &lfilename, uint retryCount)
                 if (!check_permissions(filename))
                     break;
 
-                VERBOSE(VB_IMPORTANT,
-                        QString("Could not open %1.  %2 retries remaining.")
-                        .arg(filename).arg(openAttempts));
-
-                usleep(500000);
+                lasterror = 1;
+                usleep(1000);
             }
             else
             {
                 int ret = read(fd2, buf, kReadTestSize);
                 if (ret != (int)kReadTestSize)
                 {
-                    VERBOSE(VB_IMPORTANT, LOC +
-                            QString("Invalid file (fd %1) when opening '%2'.")
-                            .arg(fd2).arg(filename) + 
-                            QString(" %2 retries remaining.")
-                            .arg(openAttempts));
-
+                    lasterror = 2;
                     close(fd2);
                     fd2 = -1;
-                    usleep(500000);
+                    usleep(1000);
                 }
                 else
                 {
                     lseek(fd2, 0, SEEK_SET);
                     openAttempts = 0;
+                    break;
                 }
             }
         }
+
+        switch (lasterror)
+        {
+            case 1:
+                VERBOSE(VB_IMPORTANT, LOC +
+                        QString("Could not open %1.").arg(filename));
+                break;
+            case 2:
+                VERBOSE(VB_IMPORTANT, LOC +
+                        QString("Invalid file (fd %1) when opening '%2'.")
+                        .arg(fd2).arg(filename));
+                break;
+            default:
+                break;
+        }
+
 
         QFileInfo fileInfo(filename);
         if (fileInfo.lastModified().secsTo(QDateTime::currentDateTime()) >
@@ -282,6 +297,9 @@ void RingBuffer::OpenFile(const QString &lfilename, uint retryCount)
     ateof = false;
     commserror = false;
     numfailures = 0;
+
+    rawbitrate = 4000;
+    CalcReadAheadThresh();
 }
 
 /** \fn RingBuffer::IsOpen(void) const
@@ -517,6 +535,7 @@ void RingBuffer::UpdatePlaySpeed(float play_speed)
 void RingBuffer::CalcReadAheadThresh(void)
 {
     const uint KB32  =  32*1024;
+    const uint KB64  =  64*1024;
     const uint KB128 = 128*1024;
     const uint KB256 = 256*1024;
     const uint KB512 = 512*1024;
@@ -530,7 +549,9 @@ void RingBuffer::CalcReadAheadThresh(void)
     wantseek       = false;
     readsallowed   = false;
     fill_min       = 1;
-    readblocksize  = (estbitrate > 9000)  ? KB256 : KB128;
+    readblocksize  = (estbitrate > 2500)  ? KB64  : KB32;
+    readblocksize  = (estbitrate > 5000)  ? KB128 : readblocksize;
+    readblocksize  = (estbitrate > 9000)  ? KB256 : readblocksize;
     readblocksize  = (estbitrate > 18000) ? KB512 : readblocksize;
 
     uint  secs_thr = 300; // seconds of buffering desired
@@ -747,6 +768,9 @@ void RingBuffer::ReadAheadThread(void)
             if (rbwpos + totfree > kBufferSize)
                 totfree = kBufferSize - rbwpos;
 
+            if (internalreadpos == 0)
+                totfree = fill_min;
+
             if (remotefile)
             {
                 if (livetvchain && livetvchain->HasNext())
@@ -852,14 +876,28 @@ long long RingBuffer::SetAdjustFilesize(void)
     return readAdjust;
 }
 
+int RingBuffer::Peek(void *buf, int count)
+{
+    // really only works with readahead, but DVD doesn't readahead
+    if (!readaheadrunning)
+    {
+        int ret = Read(buf, count);
+        Seek(0, SEEK_SET);
+        return ret;
+    }
+
+    return ReadFromBuf(buf, count, true);
+}
+
 /** \fn RingBuffer::ReadFromBuf(void*, int)
  *  \brief Reads from the read-ahead buffer, this is called by
  *         Read(void*, int) when the read-ahead thread is running.
  *  \param buf   Pointer to where data will be written
  *  \param count Number of bytes to read
+ *  \param peek  If true, don't increment read count
  *  \return Returns number of bytes read
  */
-int RingBuffer::ReadFromBuf(void *buf, int count)
+int RingBuffer::ReadFromBuf(void *buf, int count, bool peek)
 {
     if (commserror)
         return 0;
@@ -916,7 +954,7 @@ int RingBuffer::ReadFromBuf(void *buf, int count)
         if (!availWait.wait(&availWaitMutex, 250))
         {
             int elapsed = t.elapsed();
-            if  (((elapsed > 500)  && (elapsed < 750))  ||
+            if  (/*((elapsed > 500)  && (elapsed < 750))  ||*/
                  ((elapsed > 1000) && (elapsed < 1250)) ||
                  ((elapsed > 2000) && (elapsed < 2250)) ||
                  ((elapsed > 4000) && (elapsed < 4250)) ||
@@ -980,9 +1018,12 @@ int RingBuffer::ReadFromBuf(void *buf, int count)
     else
         memcpy(buf, readAheadBuffer + rbrpos, count);
 
-    readAheadLock.lock();
-    rbrpos = (rbrpos + count) % kBufferSize;
-    readAheadLock.unlock();
+    if (!peek)
+    {
+        readAheadLock.lock();
+        rbrpos = (rbrpos + count) % kBufferSize;
+        readAheadLock.unlock();
+    }
 
     if (readone)
     {

mythtv/libs/libmythtv/RingBuffer.h
@@ -50,6 +50,8 @@ class RingBuffer
     // General Commands
     void OpenFile(const QString &lfilename, uint retryCount = 12/*4*/);
     int  Read(void *buf, int count);
+    int  Peek(void *buf, int count); // only works with readahead
+
     void Reset(bool full          = false,
                bool toAdjust      = false,
                bool resetInternal = false);
@@ -99,7 +101,7 @@ class RingBuffer
     int safe_read(int fd, void *data, uint sz);
     int safe_read(RemoteFile *rf, void *data, uint sz);
 
-    int ReadFromBuf(void *buf, int count);
+    int ReadFromBuf(void *buf, int count, bool peek = false);
 
     int ReadBufFree(void) const;
     int ReadBufAvail(void) const;

mythtv/libs/libmythtv/ThreadedFileWriter.cpp
@@ -118,6 +118,7 @@ ThreadedFileWriter::ThreadedFileWriter(const QString &fname,
     tfw_min_write_size(0),
     // buffer position state
     rpos(0),                             wpos(0),
+    written(0),
     // buffer
     buf(NULL),                           tfw_buf_size(0)
 {
@@ -327,7 +328,7 @@ void ThreadedFileWriter::SyncLoop(void)
 {
     while (!in_dtor)
     {
-        bufferSyncWait.wait(1000);
+        bufferSyncWait.wait(written > tfw_min_write_size ? 1000 : 100);
         Sync();
     }
 }
@@ -337,7 +338,8 @@ void ThreadedFileWriter::SyncLoop(void)
  */
 void ThreadedFileWriter::DiskLoop(void)
 {
-    uint size = 0, written = 0;
+    uint size = 0;
+    written = 0;
 
     while (!in_dtor || BufUsed() > 0)
     {

mythtv/libs/libmythtv/ThreadedFileWriter.h
@@ -52,6 +52,7 @@ class ThreadedFileWriter
     uint            rpos;    ///< points to end of data written to disk
     uint            wpos;    ///< points to end of data added to buffer
     QMutex          buflock; ///< lock needed to update rpos and wpos
+    long long       written;
 
     // buffer
     char           *buf;

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -800,7 +800,7 @@ int AvFormatDecoder::OpenFile(RingBuffer *rbuffer, bool novideo,
 
     fmt->flags &= ~AVFMT_NOFILE;
 
-    if (!ringBuffer->isDVD())
+    if (!ringBuffer->isDVD() && !livetv)
         av_estimate_timings(ic);
 
     av_read_frame_flush(ic);
@@ -2458,7 +2458,8 @@ bool AvFormatDecoder::GetFrame(int onlyvideo)
 
         if (gotvideo)
         {
-            if (lowbuffers && onlyvideo == 0 && lastapts < lastvpts + 100 &&
+            if (lowbuffers && onlyvideo == 0 && 
+                lastapts < lastvpts + 100 &&
                 lastapts > lastvpts - 10000 && 
                 !ringBuffer->InDVDMenuOrStillFrame())
             {
@@ -2696,7 +2697,8 @@ bool AvFormatDecoder::GetFrame(int onlyvideo)
 
                     if (skipaudio)
                     {
-                        if (lastapts < lastvpts - 30 || lastvpts == 0)
+                        if ((lastapts < lastvpts - (10.0 / fps)) || 
+                            lastvpts == 0)
                         {
                             ptr += len;
                             len = 0;

commit 2bee63119a7e054b87c18b42179635adf0d0cbc0

Relax the 'we found everything' criteria slightly.  Might have caused audio to not show up with dvb occasionally caused by r9820.
git-svn-id: http://svn.mythtv.org/trunk@9825 4b91aca1-2a1f-0410-a73a-a78f2df169e8

mythtv/libs/libavformat/utils.c
@@ -2013,7 +2013,7 @@ static int try_decode_frame(AVStream *st, const uint8_t *data, int size)
  */
 int av_find_stream_info(AVFormatContext *ic)
 {
-    int i, count, ret, read_size, j, read_packets;
+    int i, count, ret, read_size, j, read_packets = 0;
     AVStream *st;
     AVPacket pkt1, *pkt;
     AVPacketList *pktl=NULL, **ppktl;
@@ -2085,7 +2085,7 @@ int av_find_stream_info(AVFormatContext *ic)
             }
         } else {
             /* we did not get all the codec info, but we read too much data */
-            if (read_size >= MAX_READ_SIZE || read_packets >= MAX_FRAMES) {
+            if (read_size >= MAX_READ_SIZE) {
                 ret = count;
                 break;
             }